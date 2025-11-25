a545d769dfa6274bc9aea1eead42ac8c3c1307f5
qmcclellan <qmcclellan@gmail.com>
2025-11-25 10:44:57 -0500

    Fixed Structure finally

--- src/main/java/com/starkgrid/demeter/Records/DemeterRegistry.java
+++ src/main/java/com/starkgrid/demeter/Records/DemeterRegistry.java
@@ -1,17 +1,7 @@
 package com.starkgrid.demeter.Records;
 
-import java.time.Instant;
 
 public record DemeterRegistry() {
 
-    String id;
-    String fileName;
-    String  absolutePath;
-    String folderPath;
-    long sizeBytes;
-    Instant createdAt;
-    Instant modifiedAt;
-    List<String> tags;
-    String vectorId;
-        
+
 }
