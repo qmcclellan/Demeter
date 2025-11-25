cc2bbf779f6acd8c6734f7ee3f8ac6cd1e87f21c
qmcclellan <qmcclellan@gmail.com>
2025-11-24 20:51:39 -0500

    Fixed folder structure

--- src/main/java/com/starkgrid/demeter/FileExecutor.java
+++ src/main/java/com/starkgrid/demeter/FileExecutor.java
@@ -1,6 +1,6 @@
 import java.util.concurrent.ExecutorService;
 
-@Component
+
 public class FileExecutor {
 
 ExecutorService executorService;
--- src/main/java/com/starkgrid/demeter/FileWatcherService.java
+++ src/main/java/com/starkgrid/demeter/FileWatcherService.java
@@ -1,75 +1,60 @@
-package main.java.com.starkgrid.demeter;
+package com.starkgrid.demeter;
 
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardWatchEventKinds;
-import java.nio.file.WatchEvent;
-import java.nio.file.WatchKey;
-import java.nio.file.WatchService;
-import java.util.List;
+import java.io.IOException;
+import java.nio.file.*;
+import org.springframework.stereotype.Component;
 
 @Component
 public class FileWatcherService {
 
-public enum Directories{
+    public enum Directories {
+        FOLDER1("/workspace/Angular"),
+        FOLDER2("/workspace/Java/SpringBoot");
 
-    folder1("/workspace/Angular"), folder2("/workspace/Java/SprinBoot");
+        private final String path;
 
-    private final String path;
-
-    Directories(String path){
-       this.path = path;
-    }
-
-    public String getPath(){
-
-        return path;
-    }
-}
-
-WatchService watchService;
-
-List<String> directories = Directories.values();
-
-
-public FileWatcherService(WatchService watchService) {
-    this.watchService = FileSystems.getDefault().newWatchService();
-}
-
-public void registerPath(String directory){
-
-    Path filePath = Paths.get(directory);
-
-    filePath.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_DELETE);
-
-
-}
-  
-
-public void startWatching(){
-
-    for(Directories d : Directories.values()){
-
-        registerPath(Paths.get(d.getPath()));
-    }
-
-    watcherLoop();
-
-}
-
-public void watcherLoop(){
-
-     while (true) {
-        WatchKey key = watchService.take();
-
-        for (WatchEvent<?> event : key.pollEvents()) {
-            Path changed = (Path) event.context();
-            System.out.println("EVENT: " + event.kind() + " on " + changed);
+        Directories(String path) {
+            this.path = path;
         }
 
-        key.reset();
+        public String getPath() {
+            return path;
+        }
     }
-}
 
+    private final WatchService watchService;
+
+    public FileWatcherService() throws IOException {
+        this.watchService = FileSystems.getDefault().newWatchService();
+    }
+
+    public void registerPath(Path path) throws IOException {
+        path.register(
+            watchService,
+            StandardWatchEventKinds.ENTRY_CREATE,
+            StandardWatchEventKinds.ENTRY_MODIFY,
+            StandardWatchEventKinds.ENTRY_DELETE
+        );
+    }
+
+    public void startWatching() throws IOException, InterruptedException {
+        for (Directories d : Directories.values()) {
+            registerPath(Paths.get(d.getPath()));
+        }
+
+        watcherLoop();
+    }
+
+    public void watcherLoop() throws InterruptedException {
+        while (true) {
+            WatchKey key = watchService.take();
+
+            for (WatchEvent<?> event : key.pollEvents()) {
+                Path changed = (Path) event.context();
+                System.out.println("EVENT: " + event.kind() + " on " + changed);
+            }
+
+            key.reset();
+        }
+    }
 }
